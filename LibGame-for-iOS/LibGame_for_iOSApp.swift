be3a439406852cd7a68f2ac9da7f19b26e5ecfd0
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-04-22 10:47:26 +0200

    first commit 🚀

---
+++ LibGame-for-iOS/LibGame_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibGame_for_iOSApp.swift
+//  LibGame-for-iOS
+//
+//  Created by Kacper Grabiec on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LibGame_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# **LibGame for iOS**
+
+An iOS version of LibGame app.
+
+I am using the free games API from[FreeToGame.com](https://www.freetogame.com/api-doc).
