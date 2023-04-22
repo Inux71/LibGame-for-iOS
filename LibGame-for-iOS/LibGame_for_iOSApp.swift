439567a1ed9717828cabba83f06f9d0a504b9c87
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-04-22 10:52:54 +0200

    add login view 🔑

--- LibGame-for-iOS/LibGame_for_iOSApp.swift
+++ LibGame-for-iOS/LibGame_for_iOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LibGame_for_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
---
+++ LibGame-for-iOS/view/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  LibGame-for-iOS
+//
+//  Created by Kacper Grabiec on 22/04/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("LibGame")
+                .font(.system(size: 32, weight: .bold))
+            
+            Button("Sign In") {
+                
+            }
+        }
+        .buttonStyle(.borderedProminent)
+    }
+}
